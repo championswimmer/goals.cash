d7906a404955cab011cc94f26bbb489c2d38c566
Arnav Gupta <dev@championswimmer.in>
2025-04-01 22:29:59 +0100

    basic skeleton of portfolio

--- lib/src/Portfolio.ts
+++ lib/src/Portfolio.ts
@@ -1,3 +1,53 @@
+import { Asset, Expense, Income, Liability } from ".";
+
 export class Portfolio {
+  startYear: number;
+  endYear: number; 
+  currentYear: number; 
+  currentAge: number; 
+
+  assets: Asset[] = [];
+  liabilities: Liability[] = [];
+  incomes: Income[] = [];
+  expenses: Expense[] = [];
+
+  constructor(startYear: number, endYear: number, currentYear: number, currentAge: number) {
+    this.startYear = startYear;
+    this.endYear = endYear;
+    this.currentYear = currentYear;
+    this.currentAge = currentAge;
+  }
+
+  addAsset(asset: Asset) {
+    if (asset.initYear < this.startYear) {
+      throw new Error("Asset cannot be added before the start year");
+    }
+    this.assets.push(asset);
+  }
+
+  addLiability(liability: Liability) {
+    if (liability.initYear < this.startYear) {
+      throw new Error("Liability cannot be added before the start year");
+    }
+    this.liabilities.push(liability);
+  }
+
+  addIncome(income: Income) {
+    if (income.initYear < this.startYear) {
+      throw new Error("Income cannot be added before the start year");
+    }
+    this.incomes.push(income);
+  }
+
+  addExpense(expense: Expense) {
+    if (expense.initYear < this.startYear) {
+      throw new Error("Expense cannot be added before the start year");
+    }
+    this.expenses.push(expense);
+  }
   
+  
+
+
+ 
 }
